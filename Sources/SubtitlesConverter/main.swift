fab3181e5c9f3c5b3ae11bafc56142ceab8017d3
Iulian Onofrei <iulian.onofrei@yahoo.com>
2021-03-07 15:25:36 +0200

    Added draft converted file downloading logic

--- Sources/SubtitlesConverter/main.swift
+++ Sources/SubtitlesConverter/main.swift
@@ -18,7 +18,7 @@ struct ContentView: View {
 			])
 			Button("Convert") {
 				let document = JSObject.global.document
-				let input = document.getElementById("file")
+				var input = document.getElementById("file")
 
 				guard let file = input.files.item(0).object else {
 					return
@@ -33,6 +33,22 @@ struct ContentView: View {
 				}
 
 				jsPromise.then { value in
+					guard let encodeURIComponent = JSObject.global.encodeURIComponent.function else {
+						return
+					}
+
+					let newContent = "new text"
+					let newContentEncoded = encodeURIComponent(newContent)
+					let newFile = "data:text/plain;charset=utf-16,\(newContentEncoded)"
+					let newFileName = "converted_\(file.name)"
+
+					let anchor = document.createElement("a")
+					_ = anchor.setAttribute("href", newFile)
+					_ = anchor.setAttribute("download", newFileName)
+					_ = anchor.click()
+
+					input.value = ""
+
 					// Without this, `jsPromise` gets released before this
 					// closure gets called.
 					print(jsPromise)
