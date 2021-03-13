e42488a0e199508927ae8911dbfa2ba7cf35464a
Iulian Onofrei <iulian.onofrei@yahoo.com>
2021-03-13 18:55:52 +0200

    Test: Added more conversions tests

--- Tests/ConvertersTests/SubtitleTimestampFormatterTests.swift
+++ Tests/ConvertersTests/SubtitleTimestampFormatterTests.swift
@@ -15,7 +15,11 @@ final class SubtitleTimestampFormatterTests: XCTestCase {
 	static var allTests = [
 		("test1", test1),
 		("test2", test2),
-		("test3", test3)
+		("test3", test3),
+		("test4", test4),
+		("test5", test5),
+		("test6", test6),
+		("test7", test7)
 	]
 
 	func test1() throws {
@@ -34,6 +38,34 @@ final class SubtitleTimestampFormatterTests: XCTestCase {
 
 	func test3() throws {
 		let original = "00:00:00,100"
+		let converted = try XCTUnwrap(original.getSubtitleTimestampDate()?.getSubtitleTimestampString())
+
+		XCTAssertEqual(converted, original)
+	}
+
+	func test4() throws {
+		let original = "00:00:00,000"
+		let converted = try XCTUnwrap(original.getSubtitleTimestampDate()?.getSubtitleTimestampString())
+
+		XCTAssertEqual(converted, original)
+	}
+
+	func test5() throws {
+		let original = "00:00:00,011"
+		let converted = try XCTUnwrap(original.getSubtitleTimestampDate()?.getSubtitleTimestampString())
+
+		XCTAssertEqual(converted, original)
+	}
+
+	func test6() throws {
+		let original = "00:00:01,000"
+		let converted = try XCTUnwrap(original.getSubtitleTimestampDate()?.getSubtitleTimestampString())
+
+		XCTAssertEqual(converted, original)
+	}
+
+	func test7() throws {
+		let original = "00:00:01,111"
 		let converted = try XCTUnwrap(original.getSubtitleTimestampDate()?.getSubtitleTimestampString())
 
 		XCTAssertEqual(converted, original)
