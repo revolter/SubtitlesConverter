79b34ee45aec7e117cd2b492cf23af6f1ead4b45
Iulian Onofrei <iulian.onofrei@yahoo.com>
2021-03-13 19:28:04 +0200

    Refactor: Extracted Content view to a separate file

---
+++ Sources/SubtitlesConverter/ContentView.swift
@@ -0,0 +1,67 @@
+//
+//  ContentView.swift
+//  
+//
+//  Created by Iulian Onofrei on 13.03.2021.
+//
+
+import JavaScriptKit
+import TokamakDOM
+
+import Converters
+
+struct ContentView: View {
+	var body: some View {
+		VStack {
+			HTML("input", [
+				"id": "file",
+				"type": "file"
+			])
+			Button("Convert") {
+				let document = JSObject.global.document
+				var input = document.getElementById("file")
+
+				guard let file = input.files.item(0).object else {
+					return
+				}
+
+				guard let promise = file.text?().object else {
+					return
+				}
+
+				guard let jsPromise = JSPromise<JSValue, Error>(promise) else {
+					return
+				}
+
+				jsPromise.then { value in
+					guard let encodeURIComponent = JSObject.global.encodeURIComponent.function else {
+						return
+					}
+
+					guard let content = value.string else {
+						return
+					}
+
+					guard let newContent = ExtenderConverter.convert(content) else {
+						return
+					}
+
+					let newContentEncoded = encodeURIComponent(newContent)
+					let newFile = "data:text/plain;charset=utf-16,\(newContentEncoded)"
+					let newFileName = "converted_\(file.name)"
+
+					let anchor = document.createElement("a")
+					_ = anchor.setAttribute("href", newFile)
+					_ = anchor.setAttribute("download", newFileName)
+					_ = anchor.click()
+
+					input.value = ""
+
+					// Without this, `jsPromise` gets released before this
+					// closure gets called.
+					print(jsPromise)
+				}
+			}
+		}
+	}
+}
